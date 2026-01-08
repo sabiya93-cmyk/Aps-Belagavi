85de3ecd0e5046a0ea9c322dafd62ba36894ea22
sabiya93-cmyk <sabiya93@gmail.com>
2026-01-08 14:22:43 +0530

    Update Firebase configuration for new project
    Updated Firebase configuration with new project details.

--- script.js
+++ script.js
@@ -2,9 +2,9 @@
 // IMPORTANT: Replace with your actual Firebase project keys
 const firebaseConfig = {
      apiKey: "YOUR_API_KEY_HERE",
-    authDomain: "YOUR_PROJECT.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT.appspot.com",
+    authDomain: "aps-belagavi.firebaseapp.com",
+    projectId: "aps-belagavi",
+    storageBucket: "aps-belagavi.appspot.com",
     messagingSenderId: "SENDER_ID",
     appId: "APP_ID"
 };
